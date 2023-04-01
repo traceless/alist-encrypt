1a359e93d7b5bcc3853fdf568af49f207ce8738d
doctor <360434467@qq.com>
2023-04-01 05:06:47 +0000

    文件加密完成后再退出

--- cli-util/index.js
+++ cli-util/index.js
@@ -7,7 +7,7 @@ import FlowEnc from './utils/flowEnc.js'
 import searchFile from './utils/searchFile.js'
 
 //
-function enccrypt(password, encType, enc, encPath) {
+async function enccrypt(password, encType, enc, encPath) {
   encPath = path.join(process.cwd(), encPath)
   const outPath = process.cwd() + '/outFile/' + Date.now()
   console.log('you input:', password, encType, enc, encPath)
@@ -21,6 +21,8 @@ function enccrypt(password, encType, enc, encPath) {
   }
   // 输入文件路径
   const allFilePath = searchFile(encPath)
+
+  const promiseArr = []
   for (const fileInfo of allFilePath) {
     const { filePath, size } = fileInfo
     const absPath = filePath.replace(encPath, '')
@@ -34,14 +36,28 @@ function enccrypt(password, encType, enc, encPath) {
     console.log('@@outFilePath', outFilePath, encType, size)
     const writeStream = fs.createWriteStream(outFilePath)
     const readStream = fs.createReadStream(filePath)
-    readStream.pipe(enc === 'enc' ? flowEnc.encryptTransform() : flowEnc.decryptTransform()).pipe(writeStream)
+    const promise = new Promise((resolve, reject) => {
+      readStream.pipe(enc === 'enc' ? flowEnc.encryptTransform() : flowEnc.decryptTransform()).pipe(writeStream)
+      readStream.on('end', () => {
+        console.log('@@finish filePath', filePath)
+        resolve()
+      })
+    })
+    promiseArr.push(promise)
   }
+  await Promise.all(promiseArr)
 }
 const arg = process.argv.slice(2)
+// check finish
+const interval = setInterval(() => {
+  console.log('waiting finish!!!')
+}, 1000)
+
 if (arg.length > 3) {
-  enccrypt(...arg)
+  enccrypt(...arg).then((res) => {
+    clearInterval(interval)
+    console.log('all file finish enc!!!')
+  })
 } else {
   console.log('input error， example param:nodejs-linux passwd12345 rc4 enc /home/myfolder ')
 }
-
-console.log('finish!!!')
