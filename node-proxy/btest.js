271d460b057e9714086e914464223c299755740f
doctor <360434467@qq.com>
2023-04-07 08:25:57 +0000

    添加crc6-8校验算法

--- node-proxy/btest.js
+++ node-proxy/btest.js
@@ -4,62 +4,11 @@ import path from 'path'
 import ChaCha20 from './src/utils/chaCha20.js'
 import ChaCha20Poly from './src/utils/chaCha20Poly.js'
 import { chownSync, copyFileSync } from 'fs'
-import nedb from './src/utils/nedb.js'
+import CRCN from './src/utils/crc6-8.js'
 
-// "Class" for calculating CRC8 checksums...
-function CRC8(polynomial, initialValue) {
-  // constructor takes an optional polynomial type from CRC8.POLY
-  if (polynomial == null) polynomial = CRC8.POLY.CRC8_CCITT
-  this.table = CRC8.generateTable(polynomial)
-  this.initial_value = initialValue
-}
+const crc6 = new CRCN(8)
 
-// Returns the 8-bit checksum given an array of byte-sized numbers
-CRC8.prototype.checksum = function (byteArray) {
-  var c = this.initial_value
+const byteBuf = Buffer.from('abcd')
 
-  for (var i = 0; i < byteArray.length; i++) {
-    c = this.table[(c ^ byteArray[i]) % 256]
-  }
-
-  return c
-}
-
-// returns a lookup table byte array given one of the values from CRC8.POLY
-CRC8.generateTable = function (polynomial) {
-  var csTable = [] // 256 max len byte array
-
-  for (var i = 0; i < 256; ++i) {
-    var curr = i
-    for (var j = 0; j < 8; ++j) {
-      if ((curr & 0x80) !== 0) {
-        curr = ((curr << 1) ^ polynomial) % 256
-      } else {
-        curr = (curr << 1) % 256
-      }
-    }
-    csTable[i] = curr
-  }
-
-  return csTable
-}
-
-// This "enum" can be used to indicate what kind of CRC8 checksum you will be calculating
-CRC8.POLY = {
-  CRC8: 0xd5,
-  CRC8_CCITT: 0x07,
-  CRC8_DALLAS_MAXIM: 0x31,
-  CRC8_SAE_J1850: 0x1d,
-  CRC_8_WCDMA: 0x9b,
-}
-var sample_text = 'Hi. I need a checksum.'
-
-// convert sample text to array of bytes
-var byte_array = sample_text.split('').map(function (x) {
-  return x.charCodeAt(0)
-})
-
-var crc8 = new CRC8()
-
-var checksum = crc8.checksum(byte_array)
-console.log(checksum & 0xff)
+const checksum = crc6.checksum(byteBuf)
+console.log(byteBuf, checksum & 0xff)
---
+++ node-proxy/src/utils/crc6-8.js
@@ -0,0 +1,106 @@
+// "Class" for calculating CRC8 checksums...
+function CRCn(num, polynomial, initialValue = 0) {
+  // constructor takes an optional polynomial type from CRC8.POLY
+  polynomial = polynomial || CRCn.POLY8.CRC8_DALLAS_MAXIM
+  if (num === 6) {
+    this.table = CRCn.generateTable6()
+  }
+  if (num === 8) {
+    this.table = CRCn.generateTable8MAXIM(polynomial)
+  }
+  this.initialValue = initialValue
+}
+
+// Returns the 8-bit checksum given an array of byte-sized numbers
+CRCn.prototype.checksum = function (byteArray) {
+  let c = this.initialValue
+  for (let i = 0; i < byteArray.length; i++) {
+    c = this.table[(c ^ byteArray[i]) % 256]
+  }
+  return c
+}
+
+// returns a lookup table byte array given one of the values from CRC8.POLY
+CRCn.generateTable8 = function (polynomial) {
+  const csTable = [] // 256 max len byte array
+  for (let i = 0; i < 256; ++i) {
+    let curr = i
+    for (let j = 0; j < 8; ++j) {
+      if ((curr & 0x80) !== 0) {
+        curr = ((curr << 1) ^ polynomial) % 256
+      } else {
+        curr = (curr << 1) % 256
+      }
+    }
+    csTable[i] = curr
+  }
+  return csTable
+}
+
+// CRC8_DALLAS_MAXIM，跟上面的比较的话，不需要输入和输出的反转
+CRCn.generateTable8MAXIM = function (polynomial) {
+  const csTable = [] // 256 max len byte array
+  for (let i = 0; i < 256; ++i) {
+    let curr = i
+    for (let j = 0; j < 8; ++j) {
+      if ((curr & 0x01) !== 0) {
+        // 0x31=>00110001 翻转 10001100=>0x8C
+        curr = ((curr >> 1) ^ 0x8c) % 256
+      } else {
+        curr = (curr >> 1) % 256
+      }
+    }
+    csTable[i] = curr
+  }
+  return csTable
+}
+
+// 已经完成了输入和输出的反转，所以输入和输出就不需要单独反转
+CRCn.generateTable6 = function () {
+  const csTable = [] // 256 max len byte array
+  for (let i = 0; i < 256; i++) {
+    let curr = i
+    for (let j = 0; j < 8; ++j) {
+      if ((curr & 0x01) !== 0) {
+        // 0x03(多项式：x6+x+1，00100011)，最高位不需要异或，直接去掉 0000 0011
+        // 0x30 = (reverse 0x03)>>(8-6) = 00110000
+        curr = ((curr >> 1) ^ 0x30) % 256
+      } else {
+        curr = (curr >> 1) % 256
+      }
+    }
+    csTable[i] = curr
+  }
+  console.log(csTable)
+  return csTable
+}
+
+CRCn.generateTable6test = function () {
+  const csTable = [] // 256 max len byte array
+  for (let i = 0; i < 256; i++) {
+    let curr = i
+    for (let j = 0; j < 8; ++j) {
+      if ((curr & 0x80) !== 0) {
+        // 0x03(多项式：x6+x+1，00100011)，最高位不需要异或，直接去掉
+        // 0x30 = (reverse 0x03) >> (8-6)
+        curr = ((curr << 1) ^ 0x03) % 256
+      } else {
+        curr = (curr << 1) % 256
+      }
+    }
+    csTable[i] = curr >> 2
+  }
+  console.log(csTable.reverse())
+  return csTable
+}
+
+// This "enum" can be used to indicate what kind of CRC8 checksum you will be calculating
+CRCn.POLY8 = {
+  CRC8: 0xd5,
+  CRC8_CCITT: 0x07,
+  CRC8_DALLAS_MAXIM: 0x31,
+  CRC8_SAE_J1850: 0x1d,
+  CRC_8_WCDMA: 0x9b,
+}
+
+export default CRCn
