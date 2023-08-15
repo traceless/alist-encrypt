d763a0591370d092e0586114aa8add4e1a5f9e73
j2rong4cn <253551464@qq.com>
2023-08-15 23:39:12 +0800

    开启webpack压缩、优化代码的功能

--- node-proxy/webpack.config.js
+++ node-proxy/webpack.config.js
@@ -1,6 +1,7 @@
 import CopyPlugin from 'copy-webpack-plugin'
 import { resolve } from 'path'
 import nodeExternals from 'webpack-node-externals'
+import TerserPlugin from 'terser-webpack-plugin'
 
 const output = {
   filename: '[name].js',
@@ -22,5 +23,31 @@ export default {
   target: 'node',
   externals: [nodeExternals()],
   mode: 'production',
-  optimization: { minimize: false },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        // 是否启用多线程
+        parallel: true,
+        // 是否将注释剥离到单独的文件中
+        extractComments: false,
+        terserOptions: {
+          // 是否压缩代码
+          compress: true,
+          // 是否压缩标识符
+          mangle: true,
+          // 是否保留函数名
+          keep_fnames: true,
+          // 是否保留类名
+          keep_classnames: true,
+          // format: {
+          //   // 输出格式化
+          //   beautify: true,
+          //   // 保留注释
+          //   comments: true,
+          // },
+        },
+      }),
+    ],
+  },
 }
