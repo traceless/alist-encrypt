078d21a2afb99b5eaa228af1396ce68c761b24d2
Yjxslhz <112344588+Yjxslhz@users.noreply.github.com>
2023-12-12 01:39:57 +0800

    Update docker_start.sh

--- node-proxy/docker_start.sh
+++ node-proxy/docker_start.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-
 # 解析环境变量 ALIST_HOST
-IFS=':' read -ra ADDR <<< "$ALIST_HOST"
-SERVER_HOST=${ADDR[0]}
-SERVER_PORT=${ADDR[1]}
+IFS=':' read -r SERVER_HOST SERVER_PORT <<EOF
+$ALIST_HOST
+EOF
 
 # 更新 config.js 文件
 sed -i "s/serverHost: '.*'/serverHost: '$SERVER_HOST'/" /node-proxy/src/config.js
